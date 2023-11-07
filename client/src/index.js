ac68442ca7fafdbdc6ee4df20e04779f40872c78
Rajan66 <rajanmaharjan042@gmail.com>
2023-11-07 22:02:04 +0545

    Added form

--- client/src/index.js
+++ client/src/index.js
@@ -4,19 +4,22 @@ import App from './App';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { legacy_createStore as createStore } from 'redux';
-import {applyMiddleware,compose} from 'redux';
-import thunk from 'redux-thunk';
+import { applyMiddleware, compose } from 'redux';
 
+import thunk from 'redux-thunk';
 import reducers from './reducers'
 
 // ReactDOM.render(<App />, document.getElementById('root'))
 
-const store = createStore(reducers,compose(applyMiddleware(thunk)))
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
 
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(
+
     <Provider store={store}>
         <App />
     </Provider>
+
 );
