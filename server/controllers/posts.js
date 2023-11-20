f4c8753f854acbed4928975be8d6e16e582fce14
Rajan66 <rajanmaharjan042@gmail.com>
2023-11-20 14:33:11 +0545

    Backend: likes and user

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -47,12 +47,23 @@ export const deletePost = async (req, res) => {
 
 export const likePost = async (req, res) => {
     const { id } = req.params;
+    if (!req.userId) return res.json({ message: "Unauthenticated" })
+
     if (!mongoose.Types.ObjectId.isValid(id)) {
         res.status(404).send('No post with that id');
     }
 
     const post = await PostMessage.findById(id);
-    const likedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
+
+    const index = post.likes.findIndex((id) => id === String(req.userId))
+
+    if (index === -1) {
+        post.likes.push(req.userId)
+    } else {
+        post.likes = post.likes.filter((id) => id !== String(req.userId))
+    }
+    const likedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true });
+
     res.json(likedPost);
 }
 
