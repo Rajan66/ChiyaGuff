e8713ddb7951a961b5db9a138d6f60b97857a71e
Rajan66 <rajanmaharjan042@gmail.com>
2023-11-28 15:21:31 +0545

    ChipInput added using AutoComplete Component

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from 'react'
-import { Container, Grow, Grid, Paper, AppBar, TextField, Button } from '@mui/material'
+import { Container, Grow, Grid, Paper, AppBar, TextField, Button, Autocomplete } from '@mui/material'
 import Posts from '../Posts/Posts'
 import Form from '../Form/Form'
 import { useNavigate, useLocation } from 'react-router-dom'
-// import ChipInput from 'material-ui-chip-input';
+import Chip from '@mui/material/Chip'
+
 
 import { useDispatch } from 'react-redux'
 import { getPosts } from '../../actions/posts'
-// import useStyles from './styles'
+import useStyles from './styles'
 
 import Pagination from '../Pagination/Pagination'
+
 
 
 function useQuery() {
@@ -17,18 +19,32 @@ function useQuery() {
 }
 
 const Home = () => {
-    // const classes = useStyles()
+    const classes = useStyles()
     const dispatch = useDispatch();
     const [currentId, setCurrentId] = useState(null);
 
     const navigate = useNavigate()
     const query = useQuery()
+
     const page = query.get('page') || 1
     const searchQuery = query.get('searchQuery')
+
+    const [search, setSearch] = useState('')
+    const [tags, setTags] = useState([])
 
     useEffect(() => {
         dispatch(getPosts());
     }, [currentId, dispatch])
+
+    const handleKeyPress = (e) => {
+        // keycode 13 means enter key.
+        if (e.keyCode === 13) {
+            //search post
+        }
+    }
+
+    const handleClick = (tag) => setTags([...tags, tag])
+    const handleDelete = (tagToDelete) => setTags(tags.filter((tag) => tag !== tagToDelete))
 
     return (
 
@@ -39,8 +55,42 @@ const Home = () => {
                         <Posts setCurrentId={setCurrentId} />
                     </Grid>
                     <Grid item xs={12} sm={6} md={3}>
-                        <AppBar  position='static' color='inherit'>
-                            <TextField name="search" variant='outlined' label="Search Memories" fullWidth value="TEST" onChange={() => { }} />
+                        <AppBar position='static' color='inherit' className={classes.appBarSearch}>
+                            <TextField
+                                name="search"
+                                variant='outlined'
+                                label="Search Memories"
+                                fullWidth value={search}
+                                onKeyUp={handleKeyPress}
+                                onChange={(e) => setSearch(e.target.value)}
+                            />
+                            <Autocomplete
+                                multiple
+                                id="tags"
+                                style={{ margin: '10px 0' }}
+                                options={[]}
+                                defaultValue={[]}
+                                freeSolo
+                                renderTags={(value, getTagProps) =>
+                                    value.map((option, index) => {
+                                        setTags(value);
+                                        return (
+                                            <Chip
+                                                key={index}
+                                                variant="outlined"
+                                                label={option}
+                                                {...getTagProps({ index })}
+                                            />
+                                        );
+                                    })
+                                }
+                                renderInput={params => (
+                                    <TextField
+                                        {...params}
+                                        label="Search Tags"
+                                    />
+                                )}
+                            />
                         </AppBar>
                         <Form currentId={currentId} setCurrentId={setCurrentId} />
                         <Paper elevation={6}>
--- client/src/components/Home/styles.js
+++ client/src/components/Home/styles.js
@@ -12,9 +12,9 @@ export default makeStyles((theme) => ({
         marginTop: '1rem',
         padding: '16px',
     },
-    gridContainer: {
-        [theme.breakpoints.down('xs')]: {
-            flexDirection: 'column-reverse'
-        }
-    }
+    // gridContainer: {
+    //     [theme.breakpoints.down('xs')]: {
+    //         flexDirection: 'column-reverse'
+    //     }
+    // }
 }))
