aeb951948e298250a8076781702968164cbc8678
Rajan66 <rajanmaharjan042@gmail.com>
2023-12-09 19:06:24 +0545

    Deployment

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://chiya-guff.onrender.com' })
+const API = axios.create({ baseURL: 'https://chiya-guff.onrender.com' })
 // https://chiya-guff.onrender.com
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
