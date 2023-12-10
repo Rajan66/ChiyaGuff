586a2b24c91c9610e9b1b5eba10ea1bef7511c6d
Rajan66 <rajanmaharjan042@gmail.com>
2023-12-10 19:02:17 +0545

    Posts sorted by updated post

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -37,7 +37,7 @@ export const getPostsBySearch = async (req, res) => {
 
 export const createPost = async (req, res) => {
     const post = req.body;
-    const newPost = new PostMessage({ ...post, creator: req.userId, createdAt: new Date().toISOString() });
+    const newPost = new PostMessage({ ...post, creator: req.userId, createdAt: new Date().toISOString(), updatedAt: new Date().toISOString() });
     try {
         await newPost.save()
         res.status(201).json(newPost);
