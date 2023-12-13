2cabb43e0cda325db4efb690f519311cf325aad4
Rajan66 <rajanmaharjan042@gmail.com>
2023-12-13 13:48:21 +0545

    Navbar responsive (working on design)

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -4,6 +4,8 @@ import logo from '../../images/tea.png'
 import { Link, useNavigate, useLocation } from "react-router-dom";
 import { useDispatch } from 'react-redux';
 import { jwtDecode } from 'jwt-decode';
+import { ReactComponent as Hamburger } from '../../images/hamburger.svg'
+import './styles.css'
 
 const Navbar = () => {
 
@@ -11,6 +13,7 @@ const Navbar = () => {
     const dispatch = useDispatch()
     const location = useLocation()
     const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+
 
     useEffect(() => {
         const token = user?.token;
@@ -27,31 +30,83 @@ const Navbar = () => {
         setUser(null)
     }
 
+    const [isNavExpanded, setIsNavExpanded] = useState(false)
+
     return (
-        <section id="home">
-            <AppBar className="appBar" style={{ flexDirection: "row", justifyContent: "space-between" }} color="inherit">
+        <nav className="navigation">
+            <a href="/" className="brand-name">
+                ChiyaGuff
+            </a>
+            <button
+                className="hamburger"
+                onClick={() => {
+                    setIsNavExpanded(!isNavExpanded)
+                }}
+            >
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    viewBox="0 0 20 20"
+                    fill="white"
+                >
+                    <path
+                        fillRule="evenodd"
+                        d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+                        clipRule="evenodd"
+                    />
+                </svg>
+            </button>
+            <div
+                className={
+                    isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
+                }
+            >
+                <ul>
 
-                <div className="brandContainer">
-                    <img className="image" src={logo} alt="icon" height="60" />
-                    <Typography component={Link} to="/posts" className="heading" variant="h3">ChiyaGuff</Typography>
-                </div>
-
-
-                <Toolbar className="toolbar"  >
                     {user ? (<div classes="profile" style={{ display: 'flex', justifyContent: "space-between" }}>
-                        <div>
-                            <Avatar alt={user.result.name} src={user.result.picture}>{user.result.name.charAt(0)}</Avatar>
-                        </div>
-                        {/* <Typography className="username" variant='h6'>{user.result.name}</Typography> */}
-
-                        <Button variant='contained' style={{ marginLeft: "20px" }} color='secondary' onClick={logout}>Logout</Button>
+                        <li>
+                            <div>
+                                <Avatar alt={user.result.name} src={user.result.picture}>{user.result.name.charAt(0)}</Avatar>
+                            </div>
+                        </li>
+                        <li>
+                            <Button variant='contained' style={{ marginLeft: "20px" }} color='secondary' onClick={logout}>Logout</Button>
+                        </li>
                     </div>) : (
-                        <Button component={Link} to='/auth' variant='contained' color='primary'>Sign In</Button>
+                        <li>
+                            <Button component={Link} to='/auth' variant='contained' color='primary'>Sign In</Button>
+                        </li>
                     )}
-                </Toolbar>
-            </AppBar>
-        </section >
-    )
+                </ul>
+            </div>
+        </nav>
+    );
+
+    // return (
+    //     <section id="home">
+    //         <AppBar className="appBar" style={{ flexDirection: "row", justifyContent: "space-between" }} color="inherit">
+
+    //             <div className="brandContainer">
+    //                 <img className="image" src={logo} alt="icon" height="60" />
+    //                 <Typography component={Link} to="/posts" className="heading" variant="h3">ChiyaGuff</Typography>
+    //             </div>
+
+
+    //             <Toolbar className="toolbar"  >
+    //                 {user ? (<div classes="profile" style={{ display: 'flex', justifyContent: "space-between" }}>
+    //                     <div>
+    //                         <Avatar alt={user.result.name} src={user.result.picture}>{user.result.name.charAt(0)}</Avatar>
+    //                     </div>
+    //                     {/* <Typography className="username" variant='h6'>{user.result.name}</Typography> */}
+
+    //                     <Button variant='contained' style={{ marginLeft: "20px" }} color='secondary' onClick={logout}>Logout</Button>
+    //                 </div>) : (
+    //                     <Button component={Link} to='/auth' variant='contained' color='primary'>Sign In</Button>
+    //                 )}
+    //             </Toolbar>
+    //         </AppBar>
+    //     </section >
+    // )
 }
 
 export default Navbar
